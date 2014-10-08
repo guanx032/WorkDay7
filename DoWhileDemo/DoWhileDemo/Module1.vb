b97c990a08a53681819c196b5c45b379d87c8eee
idsc3101 <idsc3101@gmail.com>
2014-10-08 15:25:43 -0500

    Solved

--- DoWhileDemo/DoWhileDemo/Module1.vb
+++ DoWhileDemo/DoWhileDemo/Module1.vb
@@ -1,15 +1,13 @@
-﻿Option Strict On
+﻿OOption Strict On
 
 Module Module1
 
     Sub Main()
         Dim num As Integer = 1
-
         Do While num <= 7
             Console.WriteLine(" " & num)
             num = num + 1
         Loop
-
     End Sub
 
 End Module
